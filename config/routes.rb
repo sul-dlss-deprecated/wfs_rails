c1d18d68a5bb5a81b1fc82d1d2fba16247645c09
Jack Reed <pjreed@stanford.edu>
2015-11-15 12:33:09 -0800

    standardize route formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 WfsRails::Engine.routes.draw do
-  get '/:repo/objects/:druid/lifecycle', to: 'workflow#lifecycle', constraints: { druid: /[^\/]+/ }, defaults: { format: :xml }
-  get '/:repo/objects/:druid/workflows', to: 'workflow#workflows', constraints: { druid: /[^\/]+/ }, defaults: { format: :xml }
-  get '/:repo/objects/:druid/workflows/:workflow', to: 'workflow#workflows_by_datastream', constraints: { druid: /[^\/]+/ }, defaults: { format: :xml }
+  get '/:repo/objects/:druid/lifecycle',
+      to: 'workflow#lifecycle',
+      constraints: { druid: %r{[^\/]+} },
+      defaults: { format: :xml }
+  get '/:repo/objects/:druid/workflows',
+      to: 'workflow#workflows',
+      constraints: { druid: %r{[^\/]+} },
+      defaults: { format: :xml }
+  get '/:repo/objects/:druid/workflows/:workflow',
+      to: 'workflow#workflows_by_datastream',
+      constraints: { druid: %r{[^\/]+} },
+      defaults: { format: :xml }
   put '/:repo/objects/:druid/workflows/:workflow',
       to: 'workflow#create',
       constraints: { druid: %r{[^\/]+} },
