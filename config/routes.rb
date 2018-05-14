86c5732c26e2198743d886c1831f0cb17f8857be
Justin Coyne <jcoyne@justincoyne.com>
2018-05-14 08:32:38 -0500

    DRY out the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 WfsRails::Engine.routes.draw do
-  get '/:repo/objects/:druid/lifecycle',
-      to: 'workflow#lifecycle',
-      constraints: { druid: %r{[^\/]+} },
-      defaults: { format: :xml }
-  get '/:repo/objects/:druid/workflows',
-      to: 'workflow#workflows',
-      constraints: { druid: %r{[^\/]+} },
-      defaults: { format: :xml }
-  get '/:repo/objects/:druid/workflows/:workflow',
-      to: 'workflow#workflows_by_datastream',
-      constraints: { druid: %r{[^\/]+} },
-      defaults: { format: :xml }
-  put '/:repo/objects/:druid/workflows/:workflow',
-      to: 'workflow#create',
-      constraints: { druid: %r{[^\/]+} },
-      defaults: { format: :xml }
+  scope ':repo/objects/:druid', constraints: { druid: %r{[^\/]+} }, defaults: { format: :xml } do
+    get 'lifecycle', to: 'workflow#lifecycle'
+    get 'workflows', to: 'workflow#workflows'
+    get 'workflows/:workflow', to: 'workflow#workflows_by_datastream'
+    put 'workflows/:workflow', to: 'workflow#create'
+  end
+
   get '/workflow_archive',
       to: 'workflow#archive',
       constraints: { druid: %r{[^\/]+} },
